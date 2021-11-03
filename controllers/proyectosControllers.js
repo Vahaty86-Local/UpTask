4d10ce79cda81dc9204d4324f0d33c4ce60b22c9
Vahaty <vahaty@hotmail.com>
2021-11-03 13:57:48 +0100

    Seccion 18 - 62

--- controllers/proyectosControllers.js
+++ controllers/proyectosControllers.js
@@ -1,4 +1,5 @@
 const Proyectos = require('../models/Proyectos');
+const Tareas = require("../models/Tareas");
 
 exports.proyectosHome = async (req, res) => {
     const proyectos = await Proyectos.findAll();
@@ -58,6 +59,11 @@ exports.proyectoPorUrl = async (req, res, next) => {
 
     [proyectos, proyecto] = await Promise.all([proyectosPromise, proyectoPromise]);
 
+    //Consultar tareas del proyecto
+    const tareas = await Tareas.findAll({
+        where : { proyectoId: proyecto.id }
+    });
+
     if(!proyecto) {
         return next();
     } 
@@ -65,7 +71,8 @@ exports.proyectoPorUrl = async (req, res, next) => {
     res.render('tareas', {
         nombrePagina: 'Tareas del Proyecto',
         proyectos,
-        proyecto
+        proyecto,
+        tareas
     })
 }
 
